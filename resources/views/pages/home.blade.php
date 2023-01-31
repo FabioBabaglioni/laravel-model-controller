e87d463819925d821a19bd876810ecaaa5950475
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-31 16:19:23 +0100

    Bonus 2 Layout di base

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.main-layout')
 
 @section('content')
-<h1>Titoli dei film</h1>
 
-@foreach ($movies as $movie)
-<div>
-    <h4>{{$movie['title']}}</h4>
-    <div>Original title: {{$movie['original_title']}} </div>
-    <div>nazionalità {{$movie['nationality']}}</div>
-    <div>data uscita: {{$movie['date']}}</div>
-    <div>Voto: {{$movie['vote']}}</div>
+<div class="container-fluid text-center">
+    <h1 class="py-4 text-danger">Titoli dei film</h1>
+
+    <div class="d-flex flex-wrap pb-3 justify-content-center ">
+        @foreach ($movies as $movie)
+        <div class="ms_card p-3">
+            <h4>{{$movie['title']}}</h4>
+            <div>Original title: {{$movie['original_title']}} </div>
+            <div>nazionalità {{$movie['nationality']}}</div>
+            <div>data uscita: {{$movie['date']}}</div>
+            <div>Voto: {{$movie['vote']}}</div>
+        </div>
+        @endforeach
+    </div>
 </div>
-@endforeach
+
+
 
 @endsection
