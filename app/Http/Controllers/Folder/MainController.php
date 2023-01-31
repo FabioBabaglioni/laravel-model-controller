2fb431b775ed9163edc5175d4553bcf0ab0a0a4c
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-31 15:43:37 +0100

    Collegamento pagina home

--- app/Http/Controllers/Folder/MainController.php
+++ app/Http/Controllers/Folder/MainController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Folder;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use app\Models\MyModel;
 
 class MainController extends Controller
 {
     public function index(){
-        return view('welcome');
+        return view('pages.home');
     }
 }
